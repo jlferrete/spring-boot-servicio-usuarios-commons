603399c0a0cb46a7be7afca49351ad25f058524e
José Luis Ferrete <josel.ferrete@s-dos.es>
2020-07-03 07:35:53 +0200

    Created user commons library

--- src/main/java/com/formacionbdi/springboot/app/usuarios/commons/SpringbootServicioUsuariosCommonsApplication.java
+++ src/main/java/com/formacionbdi/springboot/app/usuarios/commons/SpringbootServicioUsuariosCommonsApplication.java
@@ -1,13 +1,12 @@
 package com.formacionbdi.springboot.app.usuarios.commons;
 
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
 public class SpringbootServicioUsuariosCommonsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringbootServicioUsuariosCommonsApplication.class, args);
-	}
 
 }
